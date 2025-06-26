7656c2c4a2eb89eebbbc4d9107d27e708bbfe51a
Ava-Vispilio <163241400+Ava-Vispilio@users.noreply.github.com>
2025-06-26 08:31:14 +0800

    Add App Icons

--- BusTrackerApp/Shared/Networking/Common/Constants.swift
+++ BusTrackerApp/Shared/Networking/Common/Constants.swift
@@ -10,8 +10,8 @@
 import Foundation
 
 enum APIConstants {
-    static let arriveLahBaseURL = "http://192.168.1.250:3000"
-    static let NTUAPIBaseURL = "http://192.168.1.250:3001"
+    static let arriveLahBaseURL = "http://192.168.1.41:3000"
+    static let NTUAPIBaseURL = "http://192.168.1.41:3001"
     static let NUSAPIBaseURL = "https://nnextbus.nus.edu.sg"
     static let NUSAPIUsername = "NUSnextbus"
     static let NUSAPIPassword = "13dL?zY,3feWR^\"T"
